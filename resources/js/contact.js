6735247f6dc4ad126c55c40a65884876187d5f2a
Belen Alonso <alonsoperalbelen@gmail.com>
2023-01-27 12:38:23 +0100

    finish contact form

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -4,42 +4,57 @@
 const formulario = document.getElementById("form");
 const respuesta = document.getElementById("respuesta");
 
-formulario.addEventListener("submit", function(e) {
-    e.preventDefault();
-    console.log(`me diste un click`);
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(`me diste un click`);
 
-// new FormData lo que está haciendo es crear una nueva información del formulario
-    const datos = new FormData(formulario); 
-    console.log(datos);
+  // new FormData lo que está haciendo es crear una nueva información del formulario
+  const datos = new FormData(formulario);
+  let itsCompleted = true;
 
-    console.log(datos.get("name"));
-    console.log(datos.get("email"));
-    console.log(datos.get("tel"));
-    console.log(datos.get("message"));
-
-    // manda la información
-    fetch('https://jsonplaceholder.typicode.com/posts/1/comments',{
-        method: 'POST',
-        body: datos
-    })
-    // recibe la información
-        .then(res => res.json())
-        // devuelve la información
-        .then(datos => {
-            console.log(datos);
-            if(datos === ""){
-                respuesta.innerHTML = `
-                <div class="alert alert-danger" role="alert">
+  console.log(datos.get("name"));
+  console.log(datos.get("email"));
+  console.log(datos.get("tel"));
+  console.log(datos.get("message"));
+//   console.log(datos.getAll());
+  for (const value of datos.values()) {
+    console.log(value);
+    if(value === ""){
+        itsCompleted = false;
+    }
+  }
+  if (!itsCompleted) {
+    respuesta.innerHTML = `
+                        
+            <div class="alert alert-danger" role="alert">
                 Llena todos los campos
-      </div>
-                `
-            }else{
-                respuesta.innerHTML = `
+            </div>
+                 `;
+                 respuesta.classList.add("notfinish")
+  } else {
+    // manda la información
+    fetch("https://jsonplaceholder.typicode.com/posts/1/comments", {
+      method: "POST",
+      body: JSON.stringify({name:datos.get("name"), email:datos.get("email"), telefono:datos.get("tel"), Mensaje:datos.get("message")}),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+    })
+      // recibe la información
+      .then((res) => res.json())
+      // devuelve la información
+      .then((datos) => {
+        console.log(datos);
+        respuesta.innerHTML = `
                 <div class="alert alert-succes" role="alert">
                 Tus datos se han enviado
       </div>
-                `
-            }
-        })
-        .catch((error) => console.log(error));
-})
+                `;
+                if(respuesta.classList.contains("notfinish")){
+                    respuesta.classList.remove("notfinish")
+                }
+                respuesta.classList.add("active")
+      })
+      .catch((error) => console.log(error));
+  }
+});
